dce106e21c901fa630077a0f83ffffecb613a565
FONARMEISTER <minidoc004@gmail.com>
2022-02-14 21:06:05 +0300

    fix

--- include/ship.h
+++ include/ship.h
@@ -9,9 +9,9 @@ namespace jackal {
 
         int get_col_pos() const noexcept;
 
-        int set_row_pos() noexcept;
+        void set_row_pos() noexcept;
 
-        int set_col_pos() noexcept;
+        void set_col_pos() noexcept;
 
     private:
         int row_pos;
