6feb7a27ced5a5e492811f43fa8091537d0b95f5
fonarmeister <minidoc004@gmail.com>
2022-03-10 20:44:54 +0300

    new: closed.png open.png modify: EventWidget.h FieldWidget.h

--- include/EventWidget.h
+++ include/EventWidget.h
@@ -1,24 +1,33 @@
 #ifndef JACKALUI_EVENTWIDGET_H_
 #define JACKALUI_EVENTWIDGET_H_
+
 #include <QLabel>
 #include <QDebug>
 #include <QGridLayout>
+
 namespace jackalui {
     class EventWidget : public QWidget {
     Q_OBJECT
     public:
-        EventWidget() {
-            auto lbl = new QLabel(this);
-            QPixmap pixmap("../pics/test.png");
-            lbl->setScaledContents(true);
-            lbl->setPixmap(pixmap.scaled(150, 150, Qt::KeepAspectRatio));
+        EventWidget() : m_label(new QLabel(this)) {
+            QPixmap pixmap("../pics/closed.png");
+            m_label->setScaledContents(true);
+            m_label->setPixmap(pixmap.scaled(100, 100, Qt::KeepAspectRatio));
             auto layout = new QVBoxLayout(this);
-            layout->addWidget(lbl);
+            layout->addWidget(m_label);
+            layout->setContentsMargins(0, 0, 0, 0);
         }
 
-        int kek = 0;
     private:
-
+        void flip() {
+            if (m_is_flipped) return;
+            m_is_flipped = true;
+            QPixmap pixmap("../pics/open.png");
+            m_label->setPixmap(pixmap.scaled(100, 100, Qt::KeepAspectRatio));
+        }
+        QLabel* m_label;
+        bool m_is_flipped = false;
+    
     signals:
 
         void onPressed(EventWidget *);
@@ -26,6 +35,7 @@ namespace jackalui {
     protected:
         void mousePressEvent(QMouseEvent *event) override {
             qDebug() << "Pressed";
+            flip();
             emit onPressed(this);
         }
     };
--- include/FieldWidget.h
+++ include/FieldWidget.h
@@ -1,17 +1,20 @@
 #ifndef JACKALUI_FIELDWIDGET_H
 #define JACKALUI_FIELDWIDGET_H
+
 #include <QLabel>
 #include <QDebug>
 #include <QGridLayout>
 #include "EventWidget.h"
+
 namespace jackalui {
     class FieldWidget : public QWidget {
     Q_OBJECT
     public:
         FieldWidget() {
             auto grid = new QGridLayout(this);
-            for (int i = 0; i < 13; ++i){
-                for (int j = 0; j < 13; ++j){
+            grid->setContentsMargins(410, 10, 410, 10);
+            for (int i = 0; i < 13; ++i) {
+                for (int j = 0; j < 13; ++j) {
                     grid->addWidget(new EventWidget(), i, j);
                 }
             }
