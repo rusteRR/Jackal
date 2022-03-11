0ef37629535691e549475529073526a9d24c10cb
rusteRR <alexvingra@mail.ru>
2022-03-11 13:22:36 +0300

    new: add ship

---
+++ src/ship.cpp
@@ -0,0 +1,13 @@
+#include "ship.h"
+
+namespace jackal {
+
+    Ship::Ship(int col, int row) : m_col(col), m_row(row) {
+    }
+
+    std::pair<int, int> Ship::get_coords() {
+        return {m_col, m_row};
+    }
+
+
+}
